76dc4b995a15334290ba03d50b5d409b75198b63
Alexis <alexisvelo02@hotmail.com>
2026-05-13 10:22:43 -0300

    integracion a la clase dominio la clase expedientes

---
+++ SGE.Dominio/Expedientes/EstadoExpediente.cs
@@ -0,0 +1,9 @@
+namespace SGE.Dominio.Expedientes;
+public enum EstadoExpediente
+{
+    RecienIniciado,
+    ParaResolver,
+    ConResolucion,
+    EnNotificacion,
+    Finalizado
+}
---
+++ SGE.Dominio/Expedientes/Expediente.cs
@@ -0,0 +1,69 @@
+
+using SGE.Dominio.Comun;
+using SGE.Dominio.Tramites;
+
+namespace SGE.Dominio.Expedientes;
+
+public class Expediente
+{
+    public Guid Id { get; private set; }
+    public  Caratula Caratula { get; private set; }
+    public DateTime FechaCreacion { get; private set; }
+    public DateTime FechaUltimaModificacion { get; private set; }
+    public Guid UsuarioUltimoCambio{ get; private set; }
+    public EstadoExpediente Estado { get; private set; } 
+     
+
+    public Expediente(Caratula caratula, Guid usuarioUltimoCambio)
+    {
+        Id = Guid.NewGuid();
+        Caratula = caratula ?? throw new DominioException("La caratula no puede ser nula");
+        FechaCreacion = DateTime.Now;
+        FechaUltimaModificacion = DateTime.Now;
+        UsuarioUltimoCambio = usuarioUltimoCambio;
+        Estado = EstadoExpediente.RecienIniciado;
+    }   
+    public void ModificarCaratula(Caratula nuevaCaratula, Guid idUsuario)
+    {
+        Caratula = nuevaCaratula ?? throw new DominioException("La caratula no puede ser nula");
+        UsuarioUltimoCambio = idUsuario;
+        FechaUltimaModificacion = DateTime.Now;
+    }
+    public bool ActualizarEstado(EtiquetaTramite? ultimaEtiqueta, Guid idUsuario)
+    {
+        EstadoExpediente estadoAnterior = Estado;
+
+        if(ultimaEtiqueta == null)
+        {
+            Estado = EstadoExpediente.RecienIniciado;
+        }
+        else
+        {
+            switch (ultimaEtiqueta)
+            {
+                case EtiquetaTramite.Resolucion:
+                    Estado = EstadoExpediente.ConResolucion;
+                    break;
+                case EtiquetaTramite.PaseAEstudio:
+                    Estado = EstadoExpediente.ParaResolver;
+                    break;
+                case EtiquetaTramite.PaseAlArchivo:
+                    Estado = EstadoExpediente.Finalizado;
+                    break;
+            }
+        }
+        if(Estado == estadoAnterior)
+        {
+            return false;
+        }
+        UsuarioUltimoCambio = idUsuario;
+        FechaUltimaModificacion = DateTime.Now;
+        return true;
+    }
+    public void CambiarEstado(EstadoExpediente nuevoEstado, Guid idUsuario)
+    {
+        Estado = nuevoEstado;
+        UsuarioUltimoCambio = idUsuario;
+        FechaUltimaModificacion = DateTime.Now;
+    }
+}
