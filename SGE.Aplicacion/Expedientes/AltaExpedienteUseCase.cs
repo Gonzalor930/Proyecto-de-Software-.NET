5a73c85b93340bfe2e902094e027b048f58f953d
Alexis <alexisvelo01@hotmail.com>
2026-05-15 12:23:37 -0300

    pequeños arreglos a UseCase e Interface

--- SGE.Aplicacion/Expedientes/AltaExpedienteUseCase.cs
+++ SGE.Aplicacion/Expedientes/AltaExpedienteUseCase.cs
@@ -7,7 +7,7 @@ public class AltaExpedienteUseCase(IExpedienteRepository repositorio)
     public AgregarExpedienteResponse Ejecutar(AgregarExpedienteRequest request)
     {
         var caratulaNueva = new Caratula(request.DetalleCaratula);
-        var nuevoExpediente = new Expedientes(caratulaNueva, request.IdUsuario);
+        var nuevoExpediente = new Expediente(caratulaNueva, request.IdUsuario);
 
         repositorio.Agregar(nuevoExpediente);
 
