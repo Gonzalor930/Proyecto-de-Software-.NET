95c03e8c00a662b3900d17b76bfd512e1e52738c
Gonzalo <Gonzaromano35@gmail.com>
2026-05-16 16:55:40 -0300

    UseCase ListarTodos

---
+++ SGE.Aplicacion/Expedientes/ListarExpedientesUseCase.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using SGE.Dominio.Expedientes;
+
+namespace SGE.Aplicacion.Expedientes{
+    public class ListarExpedientesUseCase
+    {
+        private readonly IExpedienteRepository _repositorio;
+        public ListarExpedientesUseCase(IExpedienteRepository repositorio)
+        {
+            _repositorio = repositorio;
+        }
+        public ListarExpedientesResponse Ejecutar(ListarExpedientesRequest request)
+        {
+            //Busca la entidad en el repositorio
+            IEnumerable<Expediente> expedientesDominio = _repositorio.ObtenerTodos();
+
+            List<ExpedienteDTO> listaDtos = new List<ExpedienteDTO>();
+            foreach (var exp in expedientesDominio)
+            {
+                ExpedienteDTO dto = new ExpedienteDTO(
+                    exp.Id,
+                    exp.Caratula.ToString(),
+                    exp.FechaCreacion,
+                    exp.FechaUltimaModificacion,
+                    exp.Estado.ToString()
+                );
+                listaDtos.Add(dto);
+            }
+            return new ListarExpedientesResponse(listaDtos);
+        }
+    }
+}
